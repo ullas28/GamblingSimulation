c434c0f4a056a56b7ddd9d526d62de66f01167ca
unknown <you@example.com>
2022-10-02 12:53:53 +0530

    Added : Gambler resigns if he wins or loses 50 dollar

--- GamblingGame.java
+++ GamblingGame.java
@@ -10,18 +10,43 @@ public class GamblingGame {
     public static final int STAKE = 100; // GAMBLER starts with stake of $100
     public static final int BETVALUE = 1; //Gambler bets one dollar each game
 
+    static int newStake = STAKE;
+    
     /*UC2
     * As a Gambler make $1 bet so either win or loose $1*/
 
-    public static void winLoose(int r, int BETVALUE){
+    public static void winLoose(int r){
 
-        if (r == BETVALUE){
-            System.out.println("Gambler Wins!");
-        }
+        while (true) {
+            if (r == BETVALUE) {
 
-        else{
-            System.out.println("Gambler Loose!");
+                System.out.println("Gambler Wins!");
+                newStake++;
+                System.out.println("New Amount Inhand: "+newStake);
 
+                /*UC3 gambler resigns if he earns 50$ profit
+                * As a Calculative Gambler if won or lost 50% of the stake, would resign for the day*/
+
+                if (newStake == 150){
+                    System.out.println("Gambler resigns as he wins 50% more of his stake");
+                    return;
+                }
+            }
+
+            else {
+
+                System.out.println("Gambler Loose!");
+                newStake--;
+                System.out.println("New Amount Inhand: "+newStake);
+
+                /*UC3 gambler resigns if he loses 50$ profit
+                *As a Calculative Gambler if won or lost 50% of the stake, would resign for the day*/
+
+                if (newStake == 50){
+                    System.out.println("Gambler resigns as he looses 50%  of his stake");
+                    return;
+                }
+            }
         }
     }
 
@@ -31,6 +56,6 @@ public class GamblingGame {
 
         Random rand = new Random();
         int r = rand.nextInt(2);
-        winLoose(r,BETVALUE);
+        winLoose(r);
         }
     }
