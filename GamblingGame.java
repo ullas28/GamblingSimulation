06c487e022ce80c28b3dbc373b08a0ea0ec0fa31
unknown <you@example.com>
2022-10-02 11:00:49 +0530

    Added : Gambler starts with initial stake of 100 dollars

--- GamblingGame.java
+++ GamblingGame.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.Gambling;
 
 public class GamblingGame {
-        public static void main(String[] args) {
+    
+    /*UC1
+    * As a Gambler, would start with a stake of $100 every day and bet $1 every game.*/
+
+    public static final int STAKE = 100; // GAMBLER starts with stake of $100
+    public static final int BETVALUE = 1; //Gambler bets one dollar each game
+
+    public static void main(String[] args) {
 
             System.out.println("Welcome to Gambling Simulation Problem.");
         }
