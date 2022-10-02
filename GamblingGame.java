387e5d254b2a641aad5b1b8df2cb6c2fcebcefbf
unknown <you@example.com>
2022-10-02 10:47:31 +0530

    Added : Welcome program

---
+++ GamblingGame.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.Gambling;
+
+public class GamblingGame {
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Gambling Simulation Problem.");
+        }
+    }
