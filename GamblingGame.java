0cedc5fa5cd0eefe947ca2b2dfbbaa1b7ccd1a63
unknown <you@example.com>
2022-10-02 11:13:01 +0530

    Added : Gambler wins or looses 1 dollar

--- GamblingGame.java
+++ GamblingGame.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.Gambling;
 
+import java.util.Random;
+
 public class GamblingGame {
     
     /*UC1
@@ -8,8 +10,27 @@ public class GamblingGame {
     public static final int STAKE = 100; // GAMBLER starts with stake of $100
     public static final int BETVALUE = 1; //Gambler bets one dollar each game
 
+    /*UC2
+    * As a Gambler make $1 bet so either win or loose $1*/
+
+    public static void winLoose(int r, int BETVALUE){
+
+        if (r == BETVALUE){
+            System.out.println("Gambler Wins!");
+        }
+
+        else{
+            System.out.println("Gambler Loose!");
+
+        }
+    }
+
     public static void main(String[] args) {
 
-            System.out.println("Welcome to Gambling Simulation Problem.");
+        System.out.println("Welcome to Gambling Simulation Problem.");
+
+        Random rand = new Random();
+        int r = rand.nextInt(2);
+        winLoose(r,BETVALUE);
         }
     }
