5e5b5b6f5c035e63c0a6653bab0e5c11ff44f533
unknown <you@example.com>
2022-10-04 10:35:11 +0530

    Added : Gambler checks if he can play next month

--- GamblingGame.java
+++ GamblingGame.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.Gambling;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class GamblingGame {
 
@@ -16,6 +17,10 @@ public class GamblingGame {
     static int[] looseCountArr = new int[30];
     static String[] day = new String[30];
 
+    static int[] emptyArr = new int [30];
+    static int luckyDayCount = 0;
+    static int unluckyDayCount = 0;
+    static int play;
 
     /*UC2
      * As a Gambler make $1 bet so either win or loose $1*/
@@ -66,7 +71,7 @@ public class GamblingGame {
 
         }
     }
-    //After 20 days of playing every day would like to know the total amount won or lost.
+    //UC4- After 20 days of playing every day would like to know the total amount won or lost.
     //AFTER UC5 Changed 20 days to 30 days
     void monthGame () {
         for (int day = 0; day < 30; day++) {
@@ -76,20 +81,17 @@ public class GamblingGame {
             System.out.println();
         }
     }
-    /*Each month would like to know the days won and lost and by how much.*/
+    /* UC5 - Each month would like to know the days won and lost and by how much.*/
     void showWinLossCount(){
-        int count = 0;
-        int count1 = 0;
-
         for(int i=0;i<30;i++){
             if(winCountArr[i]==50) {
-                System.out.println("Day: " + (i + 1) + " Total Win count: " + (++count) + " Won by " + winCountArr[i]);
+                System.out.println("Day: " + (i + 1) + " Total Win count: " + (++luckyDayCount) + " Won by " + winCountArr[i]);
             }else
-            System.out.println("Day: "+(i+1)+ " Total Loose count: "+ (++count1) + " Lost by " + looseCountArr[i]);
+            System.out.println("Day: "+(i+1)+ " Total Loose count: "+ (++unluckyDayCount) + " Lost by " + looseCountArr[i]);
         }
     }
 
-    /*Would also like to know my luckiest day where I won maximum and my unluckiest day where I lost maximum*/
+    /*UC6 - Would also like to know my luckiest day where I won maximum and my unluckiest day where I lost maximum*/
 
     void luckydays() {
         for (int i = 0; i < 30; i++) {
@@ -110,6 +112,28 @@ public class GamblingGame {
             gmg.monthGame();
             gmg.showWinLossCount();
             gmg.luckydays();
+            /*UC7 - If won would like to continue playing next month or stop Gambling*/
+        do{
+            if(luckyDayCount>unluckyDayCount){
+                luckyDayCount = 0;
+                unluckyDayCount = 0;
+                winCountArr = looseCountArr = emptyArr;
+                System.out.println("You are doing great this month! Would you try next month too? ");
+                System.out.println("Yes? Enter 1 or press any key to quit...");
+                Scanner sc = new Scanner(System.in);
+                play = sc.nextInt();
+                if(play==1) {
+                    gmg.monthGame();
+                    gmg.showWinLossCount();
+                    gmg.luckydays();
+                }
+
+            }else{
+                System.out.println("You are out of luck and money this month! Better try Gambling later when you have enough money");
+                break;
+            }
+
+        }while(play==1);
         }
     }
 
